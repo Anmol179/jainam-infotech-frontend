a19bc87bfd63ae2aca97fab1288cecdeb0366656
Anmol179 <anmoljain834@gmail.com>
2024-01-12 13:19:59 +0530

    navbar created

---
+++ my-app/src/components/Navbar.js
@@ -0,0 +1,25 @@
+import logo from "../images/logo.png"
+
+const Navbar = () => {
+    return(
+        <div>
+            <div>
+                {/* logo */}
+                <img src={logo} alt=""></img>
+            </div>
+            <div>
+                {/* navigation */}
+                <div>CCTV</div>
+                <div>Laptop</div>
+                <div>Spare Parts</div>
+                <div>Accessories</div>
+            </div>
+            <div>
+                {/* contact */}
+                <button>Get In Touch</button>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
