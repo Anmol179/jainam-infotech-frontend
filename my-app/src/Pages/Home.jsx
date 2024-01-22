269e0a08f066f287ef7022ef91d27d9390e6c9ac
Anmol179 <anmoljain834@gmail.com>
2024-01-22 23:57:32 +0530

    Home page 80% done

--- my-app/src/Pages/Home.jsx
+++ my-app/src/Pages/Home.jsx
@@ -35,13 +35,161 @@ export default function Home() {
         >Jainam Infotech - Your Trusted Partner for Laptop and Computer Repairing, CCTV Installation, and Sales.</p>
         <img 
         style={{
-          width:"90%",
+          width:"80%",
           height:"750px",
           borderRadius:"300px",
           // overflow:"hidden"
           margin:"auto"
         }}
         src="https://025ebcb3.limecube.co/?rm=irgc&hash=025ebcb3&file=root%2Fpexels-images%2F1705040825.jpeg&fit=width&w=1920" alt="" />
+      </div>
+      <div
+      style={{
+        backgroundColor:"#14110e",
+        padding:"150px"
+      }}
+      >
+        <div 
+        style={{
+          display:"flex",
+          justifyContent:"space-between"
+        }}
+        >
+          <h1
+          style={{
+            color:"#ffffff",
+            fontSize:"50px",
+            fontWeight:"500",
+            width:"40%"
+
+          }}
+          >Welcome To Jainam Infotech</h1>
+          <p
+          style={{
+            color:"#ffffff",
+            width:"40%",
+            fontSize:"20px",
+            marginTop:"20px"
+          }}
+          >
+          At Jainam Infotech, we specialize in providing reliable solutions for all your laptop and computer needs. Whether you need repairs, CCTV installation, or want to purchase a new laptop or computer, our team of professionals is here to assist you.
+          </p>
+        </div>
+        <div
+        style={{
+          display:"flex",
+          paddingTop:"70px",
+          justifyContent:"space-between"
+        }}
+        >
+        <img 
+        style={{
+          width:"500px",
+          borderRadius:"10px",
+          height:"300px"
+        }}
+        src="https://025ebcb3.limecube.co/?rm=irgc&hash=025ebcb3&file=root%2Fpexels-images%2F1705040827.jpeg&fit=width&w=1920" alt="" />
+          <button
+          style={{
+            color:"white",
+            backgroundColor:"#375a3f",
+            height:"70px",
+            width:"180px",
+            borderRadius:"35px",
+            fontSize:"20px",
+            fontWeight:"500",
+            marginRight:"24%"
+          }}
+          >Contact Us</button>
+        </div>
+      </div>
+      <div
+      style={{
+        display:"flex",
+        // justifyContent:"space-between",
+        backgroundColor:"#241e18",
+        padding:"150px"
+      }}
+      >
+        <div
+        style={{
+          
+        }}
+        >
+          <img 
+          style={{
+            width:"450px",
+            borderRadius:"20px"
+          }}
+          src="https://025ebcb3.limecube.co/?rm=irgc&hash=025ebcb3&file=root%2Fpexels-images%2F1705040831.jpeg" alt="" />
+          <h1
+          style={{
+            color:"#ffffff",
+            fontSize:"40px",
+            fontWeight:"500"
+          }}
+          >CCTV Installation</h1>
+          <p
+          style={{
+            color:"#ffffff",
+            fontSize:"18px",
+            width:"80%",
+            fontWeight:"500",
+            paddingTop:"20px"
+          }}
+          >Professional installation of CCTV systems to enhance the security of your premises.</p>
+          <button
+          style={{
+            color:"#ffffff",
+            backgroundColor:"#375a3f",
+            height:"50px",
+            width:"180px",
+            borderRadius:"35px",
+            fontSize:"20px",
+            fontWeight:"500",
+            marginTop:"60px"
+          }}
+          >Find out more</button>
+        </div>
+        <div
+        style={{
+          paddingLeft:"60px",
+        }}
+        >
+          <img 
+          style={{
+            width:"450px",
+            borderRadius:"20px"
+          }}
+          src="https://025ebcb3.limecube.co/?rm=irgc&hash=025ebcb3&file=root%2Fpexels-images%2F1705040833.jpeg" alt="" />
+          <h1
+          style={{
+            color:"#ffffff",
+            fontSize:"40px",
+            fontWeight:"500"
+          }}
+          >Laptop & Computer Repairing</h1>
+          <p
+          style={{
+            color:"#ffffff",
+            fontSize:"18px",
+            width:"85%",
+            fontWeight:"500"
+          }}
+          >Comprehensive repair services for laptops and computers of all brands and models.</p>
+          <button
+          style={{
+            color:"#ffffff",
+            backgroundColor:"#375a3f",
+            height:"50px",
+            width:"180px",
+            borderRadius:"35px",
+            fontSize:"20px",
+            fontWeight:"500",
+            marginTop:"20px"
+          }}
+          >Find out more</button>
+        </div>
       </div>
     </div>
   )
