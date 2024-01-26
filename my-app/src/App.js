f7c3d1c25642961a8ef1e10984b90486c9c7dd4c
Anmol179 <anmoljain834@gmail.com>
2024-01-26 14:58:02 +0530

    Chat Bot added Successfully

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -12,6 +12,14 @@ import "./Styles/Chatbot.css"
 function App() {
 
   const [visible,setVisible] = useState(false);
+
+  const[name,Setname] = useState("")
+  const[phone,SetPhone] = useState("")
+  const[product,Setproduct] = useState("")
+  const[issue,Setissue] = useState("")
+
+  console.log(name)
+
   const location = useLocation();
   const path = location.pathname;
   console.log(visible);
@@ -19,10 +27,142 @@ function App() {
   let content;
 
   const steps = [{
-    id:"AA",
-    message:"Anmol jain",
-    tirger:"yesss"
-  }]
+    id:"Greet",
+    message:"Welcome to Jainam Infotech😊",
+    trigger:"inpuName"
+  },
+  {
+    id:"inpuName",
+    message:`How can I help you?`,
+    trigger:"Help"
+  },
+  {
+    id:"Help",
+    options:[
+      {
+        value:"CCTV",
+        label:"Buy CCTV",
+        trigger:"cctvInfo"
+      },
+      {
+        value:"Laptop",
+        label:"Buy Laptop",
+        trigger:"laptopInfo"
+      },
+      {
+        value:"Accessories",
+        label:"Buy Products",
+        trigger:"accessories"
+      },
+      {
+        value:"Repair",
+        label:"Get SetUp Repaired",
+        trigger:"form"
+      },
+      {
+        value:"Upgrade",
+        label:"Get SetUp Upgraded",
+        trigger:"form"
+      },
+      {
+        value:"Any Other",
+        label:"Any aother Issue",
+        trigger:"form"
+      }
+    ]
+  },
+  {
+    id:"accessories",
+    message:()=>{
+      <div>
+        <a href="http://localhost:3000/accessories" >To Get Info about Accessories Click here</a>
+      </div>
+    },
+    end:true
+  },
+  {
+    id:"cctvInfo",
+    message:()=>{
+      <div>
+        <a href="http://localhost:3000/cctv" >To Get Info about CCTV Click here</a>
+      </div>
+    },
+    end:true
+  },
+  {
+    id:"laptopInfo",
+    component:(
+      <a 
+      style={{
+        width:"100px"
+      }}
+      href="http://localhost:3000/laptop">To Get info about Laptops Click here</a>
+    ),
+    end:true
+  },
+  {
+    id:"form",
+    message:"Can i known your name",
+    trigger:"setNamee"
+  },
+  {
+    id:"setNamee",
+    user:true,
+    trigger:"setName"
+  },
+  {
+    id:"setName",
+    message: ({ steps }) => {
+      const userEnteredName = steps.setNamee.value;
+      Setname(userEnteredName);
+      return `Enter Your phone no!`;
+    },
+    trigger:"phone"
+  },
+  {
+    id:"phone",
+    user:true,
+    trigger:"setphone"
+  },
+  {
+    id:"setphone",
+    message: ({ steps }) => {
+      const userEnteredNo = steps.phone.value;
+      SetPhone(userEnteredNo);
+      return `Enter your Product Type Laptop or Computer`;
+    },
+    trigger:"enterType"
+  },
+  {
+    id:"enterType",
+    user:true,
+    trigger:"addType"
+  },
+  {
+    id:"addType",
+    message: ({ steps }) => {
+      const userEnteredType = steps.enterType.value;
+      Setproduct(userEnteredType);
+      return `Enter the upgrade/repair required in your setUp`;
+    },
+    trigger:"enterIssue"
+  },
+  {
+    id:"enterIssue",
+    user:true,
+    trigger:"thanksMsg"
+  },
+  {
+    id:"thanksMsg",
+    message: ({ steps }) => {
+      const userEnteredIssue = steps.enterIssue.value;
+      Setproduct(userEnteredIssue);
+      return `Thanks For your info, Our technical team will connect with you soon!`;
+    },
+    end:true
+  }
+
+]
 
   if(path==="/admin"){
     content = <Admin />;
@@ -37,7 +177,7 @@ function App() {
         position: "fixed",
         bottom: "25px",
         right: "25px",
-        zIndex: "999",
+        zIndex: "1",
         cursor: "pointer",
       }}
       >
@@ -46,7 +186,7 @@ function App() {
           width:"50px",
           borderRadius:"20px"
         }}
-        onClick={(()=>setVisible(true))}
+        onClick={(()=>setVisible(!visible))}
         src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtsE3cjqhoJtfPGinHPV1b6KTjloQsvC7W-SrVj7xlZg&s"  alt=""/>
         </div>
       <Footer />
@@ -57,15 +197,21 @@ function App() {
         onClick={(()=>setVisible(false))}
         style={{
           cursor:"pointer",
+          fontweight:"600",
+          fontSize:"18px",
           display:"flex",
           justifyContent:"flex-end",
-          paddingRight:"15px",
-          fontWeight:"600",
-          
+          paddingRight:"20px",
+          color:"#000000",
+          backgroundColor:"#ffffff"
         }}
         >X</h5>
         {/* Chatbot content */}
-        <ChatBot steps={steps}/>
+        <ChatBot 
+        style={{
+          backgroundColor:"#000000",
+        }}
+        steps={steps}/>
       </div>
       </>
     )
@@ -80,3 +226,4 @@ function App() {
 }
 
 export default App;
+
